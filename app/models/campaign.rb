dc9b57b363aa30970e47cf885be9bb01f884371e
shane haddock <haddocs@tcd.ie>
2026-08-06 21:12:51 +0900

    updated model relations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,8 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+
+  has_many :participations, :chapters, :notes
+  has_many :characters, through: :participations
+
+  # user_id is DM, players are participations
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,5 @@
 class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
+  has_many :characters
 end
