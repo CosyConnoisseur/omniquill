85654f1d5ade6f4dc0ee6e58b970a68843d27920
shane haddock <haddocs@tcd.ie>
2026-08-06 21:48:58 +0900

    fixing has_manys syntax

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,7 +1,9 @@
 class Campaign < ApplicationRecord
   belongs_to :user
 
-  has_many :participations, :chapters, :notes
+  has_many :participations
+  has_many :chapters
+  has_many :notes
   has_many :characters, through: :participations
 
   # user_id is DM, players are participations
