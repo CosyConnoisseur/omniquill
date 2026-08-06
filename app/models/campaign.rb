daed1d7a74d188804d86b524b4775f210ac38d38
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-08-06 22:09:09 +0900

    we now have an index page to show all campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,8 +1,8 @@
 class Campaign < ApplicationRecord
   belongs_to :user
 
-  has_many :participations, :chapters, :notes
-  has_many :characters, through: :participations
+  # has_many :participations, :chapters, :notes
+  # has_many :characters, through: :participations
 
   # user_id is DM, players are participations
 end
