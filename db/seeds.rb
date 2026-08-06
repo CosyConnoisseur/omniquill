3db3c375baab9a5f0d3ebd256c8d1082dbbcfcf0
Jared Legault <aniwhistler@gmail.com>
2026-08-06 22:15:53 +0900

    Put in the first few.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,95 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Populating users..."
+
+User.create!(
+  [
+    email: "aniwhistler@gmail.com",
+    password: "123456",
+    username: "ani"
+  ]
+)
+
+User.create!(
+  [
+    email: "jhony36@gmail.com",
+    password: "123456",
+    username: "johno37"
+  ]
+)
+
+User.create!(
+  [
+    email: "bill-flozner@hotmail.com",
+    password: "123456",
+    username: "waifulover420"
+  ]
+)
+
+User.create!(
+  [
+    email: "ra_stein@yahoo.jp",
+    password: "123456",
+    username: "goblinpounder44"
+  ]
+)
+
+puts "...finished populating users..."
+
+
+puts "Populating campaigns..."
+
+Campaign.create!(
+  [
+    title: "The great labyrinth",
+    setting: "Medieval magic fantasy. Set underground.",
+    synopsis: "Deep beneath the fractured kingdoms of Aethelgard lies the Undercrypt—a sprawling,
+    ever-shifting subterranean labyrinth forged by dead gods and ancient,
+    volatile magic. When a sudden eclipse unleashes a shadow plague that consumes the surface world,
+    an unlikely trio—a disgraced spell-thief, a guilt-ridden paladin, and a scholar who speaks the ruin-tongue
+    is forced into a desperate descent. Together, they must navigate lightless abysses, deadly arcane traps,
+    and warring subterranean civilizations to reach the maze's heart, where a forgotten engine of primordial light slumbers.
+    Yet in a realm that feeds on secrets and actively warps reality, the greatest threat isn't the horrors lurking in the dark,
+    but the horrifying truth of why the world above was cursed in the first place.",
+    user_id: 1
+  ]
+)
+
+Campaign.create!(
+  [
+    title: "Death and Determination at Camp Diamond Lake",
+    setting: "Modern day where monsters and cryptids exist. In a summer camp site surrounded by woods.",
+    synopsis: "Welcome to Camp Echo Lake, a seemingly ordinary summer getaway where the 'wildlife' includes
+    caffeinated Sasquatches, passive-aggressive Jackalopes, and a Mothman who refuses to leave the campfire light.
+    When a horde of mischief-making gnomes swipes the camp director's prized golf cart—and the emergency smores
+    stash it's up to your ragtag team of counselors to venture into the deep, weird woods and get them back.
+    Armed with rusty canoe paddles, improvised spell-casting through cafeteria snacks, and a whole lot of bad decisions,
+    you'll have to outsmart suburban cryptids to survive.
+    Just remember to roll for initiative whenever you hear rustling in the snack shack!",
+    user_id: 3
+  ]
+)
+
+puts "...finished populating campaigns..."
+
+
+puts "Populating chapters..."
+
+puts "...finished populating chapters..."
+
+
+puts "Populating characters..."
+
+puts "...finished populating characters..."
+
+
+puts "Populating notes..."
+
+puts "...finished populating notes..."
+
+
+puts "Populating stickies..."
+
+puts "...finished populating stickies..."
