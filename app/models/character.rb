78f73d2550789ff9b44789a930fcf4bfc6ee101e
shane haddock <haddocs@tcd.ie>
2026-08-06 22:30:10 +0900

    beginning characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :participation
 
-  belongs_to :user
-  belongs_to :campaign
+  # belongs_to :user
+  # belongs_to :campaign
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Character.destroy_all
+Participation.destroy_all
+Campaign.destroy_all
+User.destroy_all
+
+new_user = User.new(
+  username: "Player 1",
+  email: "p1@example.com",
+  password: "password123",
+  password_confirmation: "password123" # Devise standard check
+)
+
+new_user.save!
+
+campaign = Campaign.create!(user:new_user, title: "Epic Quest")
+first_participation = Participation.create!(user:new_user, campaign:campaign)
+Character.create!(
+  [
+    {
+    name: "Squilliam Fancyson",
+    stats_summary: "Squilliam Fancyson is Squidward Tentacles' high school arch-rival. Squilliam attended Squidward's band class, and always puts him down. He is a very wealthy, snooty rival of Squidward who looks down at Squidward, for being just a lowly cashier in a greasy spoon.",
+    participation: first_participation
+    }
+  ]
+)
