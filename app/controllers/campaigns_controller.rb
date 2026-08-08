6876e38c4ecc09da2eb8b48e471e80e275cbf14b
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-08-08 11:46:11 +0900

    fixed pundit, need to check the other crud methods

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,8 +1,7 @@
 class CampaignsController < ApplicationController
+  skip_after_action :verify_policy_scoped, only: :index
   def index
-    @campaigns = Campaign.all
-
-    #just for testing
+    @campaigns = policy_scope(Campaign)
     @characters = Character.all
   end
 end
