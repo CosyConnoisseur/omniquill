4463ffca06b0e5c2837f6aa484f1d07d7d34c28e
shane haddock <haddocs@tcd.ie>
2026-08-08 19:27:27 +0900

    mmh

--- db/cache_schema.rb
+++ db/cache_schema.rb
@@ -1,6 +1,5 @@
-class InstallSolidCache < ActiveRecord::Migration[8.1]
-  def change
-    create_table "solid_cache_entries", force: :cascade do |t|
+ActiveRecord::Schema[7.2].define(version: 1) do
+  create_table "solid_cache_entries", force: :cascade do |t|
     t.binary "key", limit: 1024, null: false
     t.binary "value", limit: 536870912, null: false
     t.datetime "created_at", null: false
@@ -9,6 +8,5 @@ class InstallSolidCache < ActiveRecord::Migration[8.1]
     t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
     t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
     t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
-  end
   end
 end
--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -1,6 +1,5 @@
-class InstallSolidQueue < ActiveRecord::Migration[8.1]
-  def change
-    create_table "solid_queue_blocked_executions", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 1) do
+  create_table "solid_queue_blocked_executions", force: :cascade do |t|
     t.bigint "job_id", null: false
     t.string "queue_name", null: false
     t.integer "priority", default: 0, null: false
@@ -127,5 +126,4 @@ class InstallSolidQueue < ActiveRecord::Migration[8.1]
   add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_recurring_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "solid_queue_scheduled_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  end
 end
