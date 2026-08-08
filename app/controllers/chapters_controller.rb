b02b13aec6581a4a228e8dece9e2c88add5f57ad
Jared Legault <aniwhistler@gmail.com>
2026-08-08 14:49:49 +0900

    Added simple editing

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,10 +1,28 @@
 class ChaptersController < ApplicationController
+  before_action :set_chapter, only: %i[show edit update]
   def index
     @campaign = Campaign.find(params[:id])
     @chapters = @campaign.chapters
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @chapter.update(chapter_params)
+    redirect_to chapter_path(@chapter)
+  end
+
+  private
+
+  def set_chapter
     @chapter = Chapter.find(params[:id])
+  end
+
+  def chapter_params
+    params.require(:chapter).permit(:title, :summary, :highlights)
   end
 end
