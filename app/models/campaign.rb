d0f64b97843eca27b533902e20eb77f6167d47eb
Ben Deng <benshendeng@gmail.com>
2026-08-11 22:30:47 +0900

    added one constraint: the tile must be at least 6 characters long

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -9,4 +9,6 @@ class Campaign < ApplicationRecord
   has_one_attached :card_image
   has_one_attached :banner
   # user_id is DM, players are participations
+
+  validates :title, length: { minimum: 6 }
 end
