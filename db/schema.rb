1cfada2ff0ef52ff0bb720d672b58a5cf3579109
Ben Deng <benshendeng@gmail.com>
2026-08-20 22:30:51 +0900

    Add transcription storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_08_111820) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_20_120642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -243,6 +243,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_08_111820) do
     t.index ["user_id"], name: "index_stickies_on_user_id"
   end
 
+  create_table "transcriptions", force: :cascade do |t|
+    t.bigint "campaign_id", null: false
+    t.datetime "created_at", null: false
+    t.string "status"
+    t.text "summary"
+    t.text "text"
+    t.datetime "updated_at", null: false
+    t.index ["campaign_id"], name: "index_transcriptions_on_campaign_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
@@ -273,4 +283,5 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_08_111820) do
   add_foreign_key "solid_queue_scheduled_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
   add_foreign_key "stickies", "chapters"
   add_foreign_key "stickies", "users"
+  add_foreign_key "transcriptions", "campaigns"
 end
