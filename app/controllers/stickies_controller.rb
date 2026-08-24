209c272c7eee5622e057341cb1b139c38483963f
Jared Legault <aniwhistler@gmail.com>
2026-08-24 20:24:39 +0900

    Stickies authorized and validated

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -3,18 +3,20 @@ class StickiesController < ApplicationController
     @sticky = Sticky.new
     @chapter = Chapter.find(params[:chapter_id])
     @campaign = Campaign.find(params[:campaign_id])
+    authorize @sticky
   end
 
   def create
     @sticky = Sticky.new(sticky_params)
     @chapter = Chapter.find(params[:chapter_id])
     @campaign = Campaign.find(params[:campaign_id])
+    authorize @sticky
     @sticky.user_id = current_user.id
     @sticky.chapter_id = @chapter.id
     if @sticky.save
       redirect_to campaign_chapter_path(@campaign, @chapter)
     else
-      render :new, :unprocessable_entity
+      redirect_back fallback_location: root_path, alert: "A minimum of 3 and a maximum of 20 characters!"
     end
   end
 
