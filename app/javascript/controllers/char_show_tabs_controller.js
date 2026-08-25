65178d310e32859c31e626399246988f18efe5c6
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-08-25 14:25:00 +0900

    got all three tabs working as intended. Click each one to view char name and profile, char description and char sheet respectively.

--- app/javascript/controllers/char_show_tabs_controller.js
+++ app/javascript/controllers/char_show_tabs_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("CHAR SHOW TABS CONNECTED!!!")
+  show(event) {
+    const tabName = event.currentTarget.dataset.tab
+
+    console.log("Clicked:", tabName)
+
+    const contents = this.element.querySelectorAll("[data-tab-content]")
+
+    contents.forEach((content) => {
+      content.style.display = "none"
+    })
+
+    const content = this.element.querySelector(
+      `[data-tab-content="${tabName}"]`
+    )
+
+    if (content) {
+      content.style.display = "block"
+    }
   }
 }
