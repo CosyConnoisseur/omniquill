73e6b9167bbdada5ff996366827a80be10951a67
shane haddock <haddocs@tcd.ie>
2026-08-25 20:02:52 +0900

    migration fix

--- db/migrate/20260822055620_fix_transcriptions_chapter_reference.rb
+++ db/migrate/20260822055620_fix_transcriptions_chapter_reference.rb
@@ -1,6 +1,6 @@
 class FixTranscriptionsChapterReference < ActiveRecord::Migration[8.1]
   def change
-    remove_reference :transcriptions, :campaign, foreign_key: true
+    remove_reference :transcriptions, :campaign, foreign_key: true, index: true
     add_reference :transcriptions, :chapter, null: false, foreign_key: true
   end
 end
