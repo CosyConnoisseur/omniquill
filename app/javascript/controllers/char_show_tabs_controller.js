bb50312b3fb3ae76c349c9fbaf7422e548b6cdd0
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-08-25 21:42:56 +0900

    fixed the monstrosity

--- app/javascript/controllers/char_show_tabs_controller.js
+++ app/javascript/controllers/char_show_tabs_controller.js
@@ -1,11 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    const savedTab = localStorage.getItem("characterTab")
+
+    if (savedTab) {
+      this.showTab(savedTab)
+    } else {
+      this.showTab("character")
+    }
+  }
+
   show(event) {
     const tabName = event.currentTarget.dataset.tab
 
-    console.log("Clicked:", tabName)
+    localStorage.setItem("characterTab", tabName)
 
+    this.showTab(tabName)
+  }
+
+  showTab(tabName) {
     const contents = this.element.querySelectorAll("[data-tab-content]")
 
     contents.forEach((content) => {
