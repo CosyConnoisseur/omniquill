3029efa90b5cfe50b9c0083525eb864d030de9a0
shane haddock <haddocs@tcd.ie>
2026-08-27 22:13:03 +0900

    added back buttons linking directly to characters

--- app/javascript/controllers/campaign_show_tabs_controller.js
+++ app/javascript/controllers/campaign_show_tabs_controller.js
@@ -48,37 +48,3 @@ export default class extends Controller {
     }
   }
 }
-
-
-// import { Controller } from "@hotwired/stimulus"
-
-// export default class extends Controller {
-//   static values = { campaignId: Number }
-
-//   connect() {
-//     console.log("connected to tabs")
-//     // 1. Build the exact key name for this specific campaign
-//     const storageKey = `campaign_tab_${this.campaignIdValue}`
-//     const savedTabId = localStorage.getItem(storageKey)
-
-//     if (savedTabId) {
-//       // 2. Safely capture the correct button element
-//       const tabTriggerEl = this.element.querySelector(`#${savedTabId}`)
-
-//       // 3. Fire Bootstrap manually only if the tab isn't already the visible active choice
-//       if (tabTriggerEl && !tabTriggerEl.classList.contains("active") && typeof bootstrap !== "undefined" && bootstrap.Tab) {
-//         const tab = bootstrap.Tab.getOrCreateInstance(tabTriggerEl)
-//         tab.show()
-//       }
-//     }
-//   }
-
-//   saveTab(event) {
-//     const activeTabId = event.target.id
-
-//     if (activeTabId) {
-//       const storageKey = `campaign_tab_${this.campaignIdValue}`
-//       localStorage.setItem(storageKey, activeTabId)
-//     }
-//   }
-// }
