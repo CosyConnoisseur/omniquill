7d950bafa2895e3f637396415e6453c903f97495
Samantha Fernandes <samsam.sammy.f@gmail.com>
2026-09-03 15:36:55 +0900

    a lot of graphic and layout updates

--- app/javascript/controllers/invite_link_controller.js
+++ app/javascript/controllers/invite_link_controller.js
@@ -21,7 +21,8 @@ export default class extends Controller {
     this.signTarget.src = "/assets/signed.png"
 
     setTimeout(() => {
-      this.element.submit()
+      this.element.closest("form").submit()
+       //finds the button_to form and submits it
     }, 1500)
   }
 }
